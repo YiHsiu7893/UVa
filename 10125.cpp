856a7d2ec913be4898dcf9a511671ee701ad7c31
Hsiu <130910974+YiHsiu7893@users.noreply.github.com>
2023-07-29 13:05:51 +0800

    Update 10125.cpp

--- 10125.cpp
+++ 10125.cpp
@@ -22,21 +22,21 @@ int main(void)
 		// brutal force
 		int a, b, c, d;
 		bool found=0;
-		for(int i=n-1; i>=0 && !found; i--)                // determine d
+		for(int i=n-1; i>=0 && !found; i--)                	      // determine d
 		{
 			d=element[i];                 
 			
-			for(int j=0; j<n && !found; j++)        	   // determine a
+			for(int j=0; j<n && !found; j++)        	      // determine a
 			{
 				a=element[j];             
 				if(a==d) continue;
 				
-				for(int k=j+1; k<n && !found; k++)  	   // determine b
+				for(int k=j+1; k<n && !found; k++)  	      // determine b
 				{
 					b=element[k];         
 					if(b==d) continue;
 					
-					for(int l=k+1; l<n && !found; l++)     // determine c
+					for(int l=k+1; l<n && !found; l++)    // determine c
 					{
 						c=element[l];
 						if(c==d) continue;
