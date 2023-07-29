26c82ee6ff0d92d086eb293106c525cc5160495c
Hsiu <130910974+YiHsiu7893@users.noreply.github.com>
2023-07-29 13:04:45 +0800

    Update 151.cpp

--- 151.cpp
+++ 151.cpp
@@ -9,28 +9,28 @@ int main(void)
 	int N, m;
 	queue<int> ans;
 	
-	cin >> N;         		 // N cities
-	while(N)			   	 // valid case
+	cin >> N;         		 		 // N cities
+	while(N)			   	 	 // valid case
 	{
-		for(m=1; m<N; m++)   // try unit m
+		for(m=1; m<N; m++)   			 // try unit m
 		{
-			int current=0;				 // start from the last step, that is, the last remaining city is numbered zero
-			for(int i=1; i<N; i++)       // 2~N -> 1~(N-1)
+			int current=0;			 // start from the last step, that is, the last remaining city is numbered zero
+			for(int i=1; i<N; i++)       	 // 2~N -> 1~(N-1)
 				current=(current+m)%i;   // Josephus Problem Equation 
 			
-			if(current==13-2)            // the last city has to be No.13-1(translate left one place)-1(array number)
+			if(current==13-2)            	 // the last city has to be No.13-1(translate left one place)-1(array number)
 			{
 				ans.push(m);
 				break;
 			}
 		}
 		
-		cin >> N;           // next case
+		cin >> N;           		 	 // next case
 	}
 	
 	while(!ans.empty())
 	{
-		cout << ans.front() << endl;     // print out all the answers
+		cout << ans.front() << endl;     	 // print out all the answers
 		ans.pop();
 	}
 	
