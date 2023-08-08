90e4b29187dfe4c7d5309200b975d8cdaa25949f
Hsiu <130910974+YiHsiu7893@users.noreply.github.com>
2023-08-08 15:33:32 +0800

    Update 948.cpp

--- 948.cpp
+++ 948.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main(void)
 {
-	int Fibonacci[40]={0, 1};   	// at most 40 digits is enough for numbers<=100,000,000
-	for(int i=2; i<40; i++)			// fill in Fibonacci series
+	int Fibonacci[40]={0, 1};   			// at most 40 digits is enough for numbers<=100,000,000
+	for(int i=2; i<40; i++)				// fill in Fibonacci series
 		Fibonacci[i]=Fibonacci[i-1]+Fibonacci[i-2];
 		
 	int N;
 	cin >> N;
-	for(int i=0; i<N; i++)			// for each case
+	for(int i=0; i<N; i++)				// for each case
 	{
 		bool flag=0;				// flag stands for the start of printing
 		
@@ -19,11 +19,11 @@ int main(void)
 		cin >> input;
 		cout << input << " = ";
 		
-		for(int j=39; j>=2; j--)	// from the largest Fibonacci number
+		for(int j=39; j>=2; j--)		// from the largest Fibonacci number
 		{
 			if(input>=Fibonacci[j])
 			{
-				flag=1;				// start printing
+				flag=1;			// start printing
 				cout << "1";
 				input-=Fibonacci[j];
 			}
