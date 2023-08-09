d4ba937124875a211ba885959291dc264fe3e384
Hsiu <130910974+YiHsiu7893@users.noreply.github.com>
2023-08-09 14:18:54 +0800

    Add files via upload

---
+++ Brick Wall Patterns.cpp
@@ -0,0 +1,21 @@
+// UVa-900: Brick Wall Patterns.cpp
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(void)
+{
+	int pattern[51]={0, 1, 2};
+	for(int i=3; i<=50; i++)
+		pattern[i]=pattern[i-1]+pattern[i-2];
+	
+	int l;
+	cin >> l;
+	while(l)
+	{
+		cout << pattern[l] << endl;
+		cin >> l;
+	}
+		
+	return 0;
+}
