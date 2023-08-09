1be022e337b4dcaeaa5a487c0afab64004c6e04b
Hsiu <130910974+YiHsiu7893@users.noreply.github.com>
2023-08-09 14:22:36 +0800

    Update 900.cpp

--- 900.cpp
+++ 900.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main(void)
 {
 	int pattern[51]={0, 1, 2};
+	// Fibonacci series
+	// pattern[i]=(pattern[i-1]+一塊垂直) + (pattern[i-2]+兩塊平行)
 	for(int i=3; i<=50; i++)
 		pattern[i]=pattern[i-1]+pattern[i-2];
 	
